39ed44261e55611b148515f4841e4aaf65e2bb82
glebooow <140659506+glebooow@users.noreply.github.com>
2025-09-20 21:10:38 +0300

    LogAlertReceiver.sol

--- LogAlertReceiver.sol
+++ LogAlertReceiver.sol
@@ -4,7 +4,6 @@ pragma solidity ^0.8.20;
 contract LogAlertReceiver {
     event BalanceAlert(string message, address indexed trap, uint256 timestamp);
 
-    // - response_function = logBalanceChange(string)
     function logBalanceChange(string calldata message) external {
         emit BalanceAlert(message, msg.sender, block.timestamp);
     }
